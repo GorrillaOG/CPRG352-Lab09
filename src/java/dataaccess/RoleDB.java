dc363137949293e00b5a2e5e5f51717c3aab4603
856622 <856622@SAIT227036IT.ACDM.DS.SAIT.CA>
2021-10-20 16:09:52 -0600

    Initial Commit

---
+++ src/java/dataaccess/RoleDB.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dataaccess;
+
+import java.util.List;
+import models.Role;
+
+/**
+ *
+ * @author 856622
+ */
+public class RoleDB {
+    public List<Role> getAll(String changepls) throws Exception {
+
+        return null;
+    }
+}
---
+++ src/java/dataaccess/UserDB.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dataaccess;
+
+import java.util.List;
+import models.User;
+
+/**
+ *
+ * @author 856622
+ */
+public class UserDB {
+    public List<User> getAll(String changepls) throws Exception {
+
+        return null;
+    }
+
+    public User get(int changepls) throws Exception {
+
+        return null;
+    }
+
+    public void insert() throws Exception {
+
+    }
+
+    public void update() throws Exception {
+
+    }
+
+    public void delete() throws Exception {
+
+    }
+}
