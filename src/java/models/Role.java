56d92d864594ed9e1bb7fcff18a91829cdaad531
Hazbleydy Cortes <90346935+hazac@users.noreply.github.com>
2021-10-26 12:56:07 -0600

    Added code to models java files

--- src/java/models/Role.java
+++ src/java/models/Role.java
@@ -5,10 +5,37 @@
  */
 package models;
 
+import java.io.Serializable;
+
 /**
  *
  * @author 856622
  */
-public class Role {
+public class Role implements Serializable{
+    private int role_id;
+    private String role_name;
     
+    public Role() {
+    }    
+
+    public int getRole_id() {
+        return role_id;
+    }
+
+    public Role(int role_id, String role_name) {
+        this.role_id = role_id;
+        this.role_name = role_name;
+    }
+    public void setRole_id(int role_id) {
+        this.role_id = role_id;
+    }
+
+    public String getRole_name() {
+        return role_name;
+    }
+
+    public void setRole_name(String role_name) {
+        this.role_name = role_name;
+    }
+
 }
