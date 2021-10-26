66326eddbe7f73a3bcffa7ece85a3bfad6d67a9c
Hazbleydy Cortes <90346935+hazac@users.noreply.github.com>
2021-10-26 12:59:02 -0600

    Added code to models java files

--- src/java/models/Role.java
+++ src/java/models/Role.java
@@ -1,31 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package models;
 
 import java.io.Serializable;
 
-/**
- *
- * @author 856622
- */
 public class Role implements Serializable{
     private int role_id;
     private String role_name;
     
     public Role() {
     }    
-
-    public int getRole_id() {
-        return role_id;
-    }
-
     public Role(int role_id, String role_name) {
         this.role_id = role_id;
         this.role_name = role_name;
     }
+    
+    public int getRole_id() {
+        return role_id;
+    }
+
     public void setRole_id(int role_id) {
         this.role_id = role_id;
     }
