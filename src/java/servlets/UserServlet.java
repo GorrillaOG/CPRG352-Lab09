a8d9cf0b876068cf0958c06f3a87e4f2153e2b66
856622 <856622@SAIT227036IT.ACDM.DS.SAIT.CA>
2021-10-29 16:44:06 -0600

    Servlet Add functionality finished

--- src/java/servlets/UserServlet.java
+++ src/java/servlets/UserServlet.java
@@ -27,18 +27,19 @@ public class UserServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         UserService service = new UserService();
-
+        
         try {
-            HttpSession session = request.getSession();
-            String email = (String) session.getAttribute("email");
+//            HttpSession session = request.getSession();
+//            String email = (String) session.getAttribute("email");
 
-            ArrayList<User> usersList = service.getAll(email);
+            ArrayList<User> usersList = service.getAll();
             //user forEach loop to display  the list
             request.setAttribute("users", usersList);
-
+            
         } catch (Exception ex) {
-
+            
         }
+        
         //achor tag and href for action 
         //should be inside the for loop as user will select the user
         //href="users?action=selected&amp;email=${account.email}"
@@ -48,33 +49,44 @@ public class UserServlet extends HttpServlet {
             User user = service.get(email);
             request.setAttribute("selectedAccount", user);
         }
+        
         getServletContext().getRequestDispatcher("/WEB-INF/users.jsp").forward(request, response);
+        return;
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        HttpSession session = request.getSession();
-        String email = (String) session.getAttribute("email");
-
         UserService service = new UserService();
         String action = request.getParameter("action");
-        String userEmail = request.getParameter("email");
-        String userFirstname = request.getParameter("firstName");
-        String userLastname = request.getParameter("lastName");
-        String userRole = request.getParameter("role");
-
+        
         if (action.equals("add")) {
-            service.insert(userEmail, userFirstname, userLastname, userRole);
-        } else if (action.equals("edit")) {
-            service.update(userEmail, userFirstname, userLastname, userRole);
-        } else if (action.equals("delete")) {
-            service.delete(email);
+            String userEmail = request.getParameter("email");
+            boolean userActive = false;
+            if (request.getParameter("active") != null) {
+                userActive = true;
+            }
+            String userFirstname = request.getParameter("firstName");
+            String userLastname = request.getParameter("lastName");
+            String userPassword = request.getParameter("password");
+            int userRole = Integer.parseInt(request.getParameter("role"));
+            
+            try {
+                service.insert(userEmail, userActive, userFirstname, userLastname, userPassword, userRole);
+                doGet(request,response);
+            }
+            catch (Exception e) {
+            }
+     
         }
+//        else if (action.equals("edit")) {
+//            service.update(userEmail, userFirstname, userLastname, userRole);
+//        } else if (action.equals("delete")) {
+//            service.delete(email);
+//        }
 
-        List<User> users = service.getAll(email);
-        request.setAttribute("Users", users);
-
-        getServletContext().getRequestDispatcher("/WEB-INF/users.jsp").forward(request, response);
+//        request.setAttribute("Users", users);
+//
+//        getServletContext().getRequestDispatcher("/WEB-INF/users.jsp").forward(request, response);
 
     }
 
